e2be1e6e780f61da921081a01c2a30fc2c21b99f
kleberkeki <kleber.barbosa93@hotmail.com>
2025-07-19 08:35:59 -0300

    criando as requisicoes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log('Hello World')
+import express, { request, response } from 'express'
+
+const app = express()
+
+
+// http://localhost:3333/users
+
+app.get('/users', (request, response) => {
+    return response.json(['usuario 1', 'usuario 2'])
+})
+
+app.post('/users', (request, response) => {
+    return response.json({ message: 'criando usuario'})
+})
+
+app.put('/users', (request, response) => {
+    return response.json({ message: 'atualizando usuario'})
+})
+
+app.delete('/users', (request, response) => {
+    return response.json({ message: 'Excluindo usuario'})
+})
+
+
+app.listen('3333' , () => {
+    console.log('back-end Started')
+} )
